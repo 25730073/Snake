19edccc9cb22819f6d0d30878678e9dc3ccff62c
25730073 <25730073@ms.uit.edu.vn>
2025-11-27 15:35:38 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#include <ctime>
 using namespace std;
 
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
 
 // Hàm di chuyển con trỏ
 void gotoxy(int column, int line){
@@ -13,63 +18,68 @@ void gotoxy(int column, int line){
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-
 // Cấu trúc toạ độ
 struct Point{ int x, y; };
 
-
-// --- LỚP RẮN (KHUNG SƯỜN) ---
+// --- LỚP RẮN ---
 class CONRAN {
-    public:
-        Point A[200]; 
-        int DoDai; 
+public:
+    Point A[200];
+    int DoDai;
 
+    CONRAN(){
+        DoDai = 3;
+        A[0].x = 10; A[0].y = 10;
+        A[1].x = 11; A[1].y = 10;
+        A[2].x = 12; A[2].y = 10;
+    }
 
-        CONRAN(){
-            DoDai = 3;
-            A[0].x = 10; A[0].y = 10;
-            A[1].x = 11; A[1].y = 10;
-            A[2].x = 12; A[2].y = 10;
+    // Vẽ rắn + mồi
+    void Ve(Point Qua){
+        for (int i = 0; i < DoDai; i++){
+            gotoxy(A[i].x, A[i].y);
+            cout << "O";
         }
+        gotoxy(Qua.x, Qua.y);
+        cout << "*";
+    }
 
+    // Di chuyển
+    void DiChuyen(int Huong, Point &Qua){
+        // Dồn thân
+        for (int i = DoDai - 1; i > 0; i--)
+            A[i] = A[i - 1];
 
-        // Vẽ thân rắn (khung sườn)
-        void Ve(Point Qua){
-            for (int i = 0; i < DoDai; i++){
-                gotoxy(A[i].x, A[i].y);
-                cout << "X";
-            }
-            gotoxy(Qua.x, Qua.y); cout<<"*";
-        }
+        // Đầu rắn
+        if (Huong == 0) A[0].x += 1;
+        if (Huong == 1) A[0].y += 1;
+        if (Huong == 2) A[0].x -= 1;
+        if (Huong == 3) A[0].y -= 1;
 
-
-        // Di chuyển (khung sườn)
-        void DiChuyen(int Huong, Point& Qua){
-            for (int i = DoDai - 1; i > 0; i--)
-                A[i] = A[i - 1];
-            if (Huong == 0) A[0].x += 1; // sang phải
-            if (Huong == 1) A[0].y += 1; // đi xuống
-            if (Huong == 2) A[0].x -= 1; // sang trái
-            if (Huong == 3) A[0].y -= 1; // đi lên
-            if ((A[0].x == Qua.x) && (A[0].y == Qua.y)){
+        // Ăn mồi
+        if (A[0].x == Qua.x && A[0].y == Qua.y){
             DoDai++;
-            Qua.x = rand()%(MAXX-MINX)+MINX;
-            Qua.y = rand()%(MAXY-MINY)+MINY;
-            }
+
+            // tạo đoạn mới nối vào đuôi
+            A[DoDai - 1] = A[DoDai - 2];
+
+            Qua.x = rand() % (MAXX - MINX - 1) + MINX + 1;
+            Qua.y = rand() % (MAXY - MINY - 1) + MINY + 1;
         }
+    }
 };
 
-#define MINX 2
-#define MINY 2
-#define MAXX 35
-#define MAXY 20
+// Vẽ khung
 void VeKhung(){
-    for (int x = MINX; x <= MAXX; x++)
-        for (int y = MINY; y <= MAXY; y++)
-            if(x== MINX || x == MAXX || y == MINY || y == MAXY){
-                gotoxy(x,y);
-                cout << "+";
-            }
+    for (int x = MINX; x <= MAXX; x++){
+        gotoxy(x, MINY); cout << "+";
+        gotoxy(x, MAXY); cout << "+";
+    }
+
+    for (int y = MINY; y <= MAXY; y++){
+        gotoxy(MINX, y); cout << "+";
+        gotoxy(MAXX, y); cout << "+";
+    }
 }
 
 int main(){
@@ -79,10 +89,11 @@ int main(){
 
     Point Qua;
     srand((int)time(0));
-    Qua.x = rand()%(MAXX-MINX)+MINX;
-    Qua.y = rand()%(MAXY-MINY)+MINY;
+    Qua.x = rand() % (MAXX - MINX - 1) + MINX + 1;
+    Qua.y = rand() % (MAXY - MINY - 1) + MINY + 1;
 
     while (1){
+        // Bắt phím
         if (_kbhit()){
             t = _getch();
             if (t == 'a') Huong = 2;
@@ -91,14 +102,14 @@ int main(){
             if (t == 's') Huong = 1;
         }
 
-
+        // Xoá màn hình
         system("cls");
-        r.Ve(Qua); // vẽ rắn
-        r.DiChuyen(Huong, Qua); // di chuyển
-        VeKhung();
+
+        VeKhung();        // vẽ khung trước
+        r.Ve(Qua);        // vẽ rắn
+        r.DiChuyen(Huong, Qua); 
         Sleep(150);
     }
-
 
     return 0;
 }
